e443d94f951179a78d127a7540265b09bce5a550
Kelly  Duong <kelly1234@gmail.com>
2017-03-17 00:22:37 -0400

    cleanup code and add download button

--- sixthTry.js
+++ sixthTry.js
@@ -1,84 +1,131 @@
 			var video = document.getElementById('video');
             var canvas = document.getElementById('canvas');
-            var context = canvas.getContext('2d');			
+            var context = canvas.getContext('2d');
+			
 
             navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.oGetUserMedia || navigator.msGetUserMedia;
 
             if(navigator.getUserMedia){
                 navigator.getUserMedia({video:true}, streamWebCam, throwError);
             }
-			//Access the camera
+
             function streamWebCam (stream) {
                 video.src = window.URL.createObjectURL(stream);
                 video.play();
             }
-			//throw an error if camera is in use/not supported
+
             function throwError () {
-                alert("Camera is not supported on this device or in use");
+                alert("Camera is in use or not supported");
             }
-            //save canvas as an image
-			function snap() {			
-				canvas.width = video.videoWidth*0.7;
-				canvas.height = video.videoHeight*0.71;
-				context.drawImage(video, 0, 0, canvas.width, canvas.height);
-			}
-			// Add event listener for Filter Button (to switch filters)
-			function none(){
-				video.className ="none";
-				video.style.none;   
-			}
-			function grayscale () {	            
-				video.className ="grayscale";
-				video.style.grayscale;    
-			}
-			function sepia () {	            
-				video.className ="sepia";
-				video.style.sepia;    
-			}
-			function blur1 () {	            
-				video.className ="blur";
-				video.style.blur1;    
-			}
-			function brightness () {
             
-				video.className ="brightness";
-				video.style.brightness;    
+			function snap() {
+			
+			canvas.width = video.videoWidth*0.7;
+			canvas.height = video.videoHeight*0.71;
+			context.drawImage(video, 0, 0, canvas.width, canvas.height);
+			}			
+			
+			function download() {
+				var dt = canvas.toDataURL('image/jpeg');
+				this.href = dt;
 			}
-			function saturate () {	            
-				video.className ="saturate";
-				video.style.saturate;    
+			downloadLnk.addEventListener('click', download, false);
+			
+			// Add event listener to effect Button (to switch video filters)
+			function none(){
+			video.className ="none";
+            video.style.none; 
+			canvas.className = "none";
+			canvas.style.none;			
 			}
-			function hueRotate () {	            
-				video.className ="huerotate";
-				video.style.hue-rotate;    
+			function grayscale () {	
+            
+			video.className ="grayscale";
+            video.style.grayscale; 
+			canvas.className = "grayscale";
+			canvas.style.grayscale;			
 			}
-			function invert () {	            
-				video.className ="invert";
-				video.style.invert;    
+			function sepia () {	
+            
+			video.className ="sepia";
+            video.style.sepia;   
+			canvas.className = "sepia";
+			canvas.style.sepia;
 			}
-			function hueRotate2 () {            
-				video.className ="huerotate2";
-				video.style.huerotate2;    
+			function blur1 () {	
+            
+			video.className ="blur";
+            video.style.blur;  
+			canvas.className = "blur";
+			canvas.style.gblur;			
 			}
-			function hueRotate3 () {	            
-				video.className ="huerotate3";
-				video.style.huerotate3;    
+			function brightness () {	
+            
+			video.className ="brightness";
+            video.style.brightness;  
+			canvas.className = "brightness";
+			canvas.style.brightness;			
+			}
+			function saturate () {	
+            
+			video.className ="saturate";
+            video.style.saturate;    
+			canvas.className = "saturate";
+			canvas.style.saturate;	
+			}
+			function hueRotate () {	
+            
+			video.className ="huerotate";
+            video.style.huerotate;  
+			canvas.className = "huerotate";
+			canvas.style.huerotate;				
+			}
+			function invert () {	
+            
+			video.className ="invert";
+            video.style.invert;  
+			canvas.className = "invert";
+			canvas.style.invert;				
+			}
+			function hueRotate2 () {	
+            
+			video.className ="huerotate2";
+            video.style.huerotate2;   
+			canvas.className = "huerotate2";
+			canvas.style.huerotate2;				
+			}
+			function hueRotate3 () {	
+            
+			video.className ="huerotate3";
+            video.style.huerotate3;    
+			canvas.className = "huerotate3";
+			canvas.style.huerotate3;		
 			}
 			
-	/* Mangage Drop Down Buttons */		
-	function myFunction() {
-		document.getElementById("myDropdown").classList.toggle("show");	
-	}
-
-	window.onclick = function(event) {
-		if (!event.target.matches('.dropbtn')) {
-			var dropdowns = document.getElementsByClassName("dropdown-content");
-			var i;
-			for (i = 0; i < dropdowns.length; i++) {
-				var openDropdown = dropdowns[i];
-				if (openDropdown.classList.contains('show')) {
-					openDropdown.classList.remove('show');
-					}
-				}
+			/* When the user clicks on the button,
+			toggle between hiding and showing the dropdown content */
+			function myFunction() {
+				document.getElementById("myDropdown").classList.toggle("show");
+	
 			}
-	};
+
+			// Close the dropdown if the user clicks outside of it
+	window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
+
+
+
+
+
+
